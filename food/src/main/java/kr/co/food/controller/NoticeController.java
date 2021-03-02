bcb9e50b2c3ce726615d4f2d978da53dc27a93dd
ahy01 <ahy01@jaeseok>
2021-03-02 10:30:58 +0900

    notice

--- food/src/main/java/kr/co/food/controller/NoticeController.java
+++ food/src/main/java/kr/co/food/controller/NoticeController.java
@@ -1,9 +1,16 @@
 package kr.co.food.controller;
 
+import java.io.IOException;
+
+import javax.servlet.http.HttpServletRequest;
+
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.oreilly.servlet.MultipartRequest;
+import com.oreilly.servlet.multipart.DefaultFileRenamePolicy;
 
 import kr.co.food.dao.NoticeDao;
 import kr.co.food.dto.NoticeDto;
@@ -18,12 +25,16 @@ public String write() {
 	return "/notice/write";
 }
 @RequestMapping("/write_ok")
-public String write_ok(NoticeDto ndto)
+public String write_ok(HttpServletRequest request) throws IOException
 {
-  NoticeDao ndao=sqlSession.getMapper(NoticeDao.class);
-  ndao.write_ok(ndto);
-  return "redirect:/notice/list";
-  
-}
+	String path=request.getRealPath("resources/img");
+	int max=1024*1024*10;
+     MultipartRequest multi=new MultipartRequest(request, path, max, "utf-8", new DefaultFileRenamePolicy());		
+     multi.getParameter("title");
+      multi.getFilesystemName("fname"); //저장되는 파일 이름
+      multi.getOriginalFileName("fname");
+      
+      return "redirect:/upload/list";
+   }
 
 }
--- food/src/main/webapp/WEB-INF/views/notice/write.jsp
+++ food/src/main/webapp/WEB-INF/views/notice/write.jsp
@@ -3,7 +3,7 @@
 <%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
 
 
-<form method="post" action="write_ok">
+<form method="post" action="write_ok" enctype="multipart/form-data">
     공지제목<input type="text" name="notice_title"><p>
     공지내용<textarea cols="60" rows="30"  name="notice_content"></textarea><p>
     공지관리자이름<input type="text" name="notice_name"><p>
